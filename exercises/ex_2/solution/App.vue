be41d6d6e64694db5ec3879bff44c5154ced8125
Vaiva_Gribauskaite1 <vaiva1@T1907.local>
2021-04-23 21:14:37 +0300

    excercises and solutions

---
+++ exercises/ex_2/solution/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app" class="cards">
+    <div v-for="card in info" v-bind:key="card.name">
+      <Card
+        :name="card.name"
+        :image="card.image"
+        :yearOfBirth="card.yearOfBirth"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue'
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  },
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('http://hp-api.herokuapp.com/api/characters')
+      .then(response => (this.info = response.data))
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.cards{
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+</style>
